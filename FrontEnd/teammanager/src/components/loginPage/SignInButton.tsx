e95fa6be3c59700a2e78209673ce8289f788d754
xMatty522 <bronczek522@gmail.com>
2023-07-10 13:25:19 +0200

    login page

---
+++ FrontEnd/teammanager/src/components/loginPage/SignInButton.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Buttons.css"
+
+const SignInButton = () => {
+    return(
+        <button>SIGN IN</button>
+    );
+};
+export default SignInButton;
--- FrontEnd/teammanager/src/pages/loginPage.tsx
+++ FrontEnd/teammanager/src/pages/loginPage.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import EmailInput from "../components/loginPage/emailInput";
 import PasswordInput from "../components/loginPage/passwordInput";
 import "./loginPage.css"
+import SignInButton from "../components/loginPage/SignInButton";
 const LoginPage = () => {
     return(
         <div id="main">
             
         <div id="loginBar">
-            Login
+            <h2>Login</h2>
             <EmailInput></EmailInput>
             <PasswordInput></PasswordInput>
+            <SignInButton></SignInButton>
+            <p>Forgot <b>Username/Password?</b><br></br>
+            Don't have an account? <b>Sign up</b></p>
         </div>
         </div>
     );
